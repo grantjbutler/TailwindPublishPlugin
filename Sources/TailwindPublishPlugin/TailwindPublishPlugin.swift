262046e3624a6a4934e6b8023362fec76b906808
Grant J. Butler <grantjbutler@users.noreply.github.com>
2023-09-10 13:53:42 -0400

    Add some additional validation.

--- Sources/TailwindPublishPlugin/TailwindPublishPlugin.swift
+++ Sources/TailwindPublishPlugin/TailwindPublishPlugin.swift
@@ -7,16 +7,12 @@ extension PublishingStep {
     public static func generateTailwindCSS(
         inputPath: Path = "Resources/styles.css",
         outputPath: Path = "styles.css",
-        configPath: Path = "tailwind.config.js"
+        configPath: Path = "tailwind.config.js",
+        packagePath: Path = "package.json"
     ) -> PublishingStep {
         return .step(named: "Generate Tailwind CSS", body: { context in
-            let packageRoot: Folder
-            do {
-                packageRoot = try context.folder(at: "/")
-            } catch {
-                throw TailwindCSSError.couldNotDetermineProjectRoot
-            }
-            
+            let npmPackageRootPath = try verifyNPMPackage(at: packagePath, context: context)
+        
             let configFile: File
             do {
                 configFile = try context.file(at: configPath)
@@ -36,14 +32,50 @@ extension PublishingStep {
             try shellOut(
                 to: ProcessInfo.processInfo.environment["NPX_BINARY", default: "npx"],
                 arguments: ["tailwindcss", "-c", configFile.path, "-i", inputFile.path, "-o", outputFile.path, "--minify"],
-                at: packageRoot.path
+                at: npmPackageRootPath
             )
         })
     }
 }
 
+private func verifyNPMPackage<Site>(at packagePath: Path, context: PublishingContext<Site>) throws -> String where Site: Website {
+    let packageFile: File
+    let package: NPMPackage
+    do {
+        packageFile = try context.file(at: packagePath)
+        let packageJSONData = try packageFile.read()
+        
+        // If we can't parse the package.json, don't let that block trying to build. We can try executing the tailwind
+        // CLI tool and if that fails, then that should fail the whole build.
+        guard let decodedPackage = try? JSONDecoder().decode(NPMPackage.self, from: packageJSONData) else {
+            return packageFile.parent!.path
+        }
+        
+        package = decodedPackage
+    } catch {
+        throw TailwindCSSError.noNPMPackage
+    }
+    
+    guard package.isDependencyInstalled("tailwindcss") else {
+        throw TailwindCSSError.tailwindNotInstalled
+    }
+    
+    return packageFile.parent!.path
+}
+
+private struct NPMPackage: Decodable {
+    let dependencies: [String: String]
+    let devDependencies: [String: String]
+    
+    func isDependencyInstalled(_ dependency: String) -> Bool {
+        return dependencies.keys.contains(dependency)
+            || devDependencies.keys.contains(dependency)
+    }
+}
+
 private enum TailwindCSSError: Error {
-    case couldNotDetermineProjectRoot
+    case noNPMPackage
+    case tailwindNotInstalled
     case noConfigFile
     case noStyles
 }
